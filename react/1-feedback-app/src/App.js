f938923ad33e8d7a9b0301c6b66104037feb148f
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2022-01-21 11:57:07 -0300

    progress is being done...

--- react/1-feedback-app/src/App.js
+++ react/1-feedback-app/src/App.js
@@ -1,9 +1,38 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
+import FeedbackForm from './components/FeedbackForm'
+import FeedbackList from './components/FeedbackList'
+import { FeedbackProvider } from './context/FeedbackContext'
+import FeedbackStats from './components/FeedbackStats'
+import Header from './components/Header'
+
 function App() {
   return (
-    <div className='container'>
-      <h1>My App</h1>
-    </div>
-  );
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route
+              exact
+              path='/'
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
+
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ react/1-feedback-app/src/components/FeedbackForm.jsx
@@ -0,0 +1,68 @@
+import { useContext, useState } from 'react'
+import Button from './shared/Button'
+import Card from './shared/Card'
+import FeedbackContext from '../context/FeedbackContext'
+import RatingSelect from './RatingSelect'
+
+function FeedbackForm() {
+  const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
+
+  const { addFeedback, feedbackEdit } = useContext(FeedbackContext)
+
+  const handleTextChange = (e) => {
+    const newText = e.target.value
+
+    if (newText === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (newText !== '' && newText.trim().length <= 10) {
+      setBtnDisabled(true)
+      setMessage('Text must be at least 10 characters')
+    } else {
+      setBtnDisabled(false)
+      setMessage(null)
+    }
+
+    setText(newText)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      }
+
+      addFeedback(newFeedback)
+
+      setText('')
+    }
+  }
+
+  return (
+    <Card>
+      <form onSubmit={handleSubmit}>
+        <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
+        <div className='input-group'>
+          <input
+            onChange={handleTextChange}
+            type='text'
+            placeholder='Write a review'
+            value={text}
+          />
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className='message'>{message}</div>}
+      </form>
+    </Card>
+  )
+}
+
+export default FeedbackForm
